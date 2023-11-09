77d2648870bb21200257fa448343cfa42c138dcc
Anneateba <98327101+Anneateba@users.noreply.github.com>
2023-11-09 01:18:51 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -69,13 +69,15 @@ if ("serviceWorker" in navigator) {
 var target = document.getElementById('target');
 var watchId;
 
-
+//This function allows us to formate and display the informations about the location 
 function appendLocation(location, verb) {
   verb = verb || 'updated';
   var newLocation = document.createElement('p');
   newLocation.innerHTML = 'Location ' + verb + ': ' + location.coords.latitude + ', ' + location.coords.longitude + '';
   target.appendChild(newLocation);
 }
+
+
 
 if ('geolocation' in navigator) {
   document.getElementById('askButton').addEventListener('click', function () {
