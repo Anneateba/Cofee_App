9616e9ca0cb0d49a4edc515fb7acd6a4613e5619
Anneateba <98327101+Anneateba@users.noreply.github.com>
2023-11-16 16:18:53 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -313,3 +313,33 @@ async function writeFile() {
   await writer.close()
   
   target.innerHTML = 'Test content written to ' + file.name + '.';
+
+
+if ('storage' in navigator && 'estimate' in navigator.storage) {
+  navigator.storage.estimate()
+    .then(estimate => {
+      document.getElementById('usage').innerHTML = estimate.usage;
+      document.getElementById('quota').innerHTML = estimate.quota;
+      document.getElementById('percent').innerHTML = (estimate.usage * 100 / estimate.quota).toFixed(0);
+    });
+}
+
+if ('storage' in navigator && 'persisted' in navigator.storage) {
+  navigator.storage.persisted()
+    .then(persisted => {
+      document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
+    });
+}
+
+function requestPersistence() {
+  if ('storage' in navigator && 'persist' in navigator.storage) {
+    navigator.storage.persist()
+      .then(persisted => {
+        document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
+      });
+  }
+}
+
+
+  
+  
